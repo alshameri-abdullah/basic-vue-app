937f379de8d75e87ca15de152f446f16b37319d4
Abdullah Al-Shameri <alshameriaf@gmail.com>
2022-03-13 02:09:12 +0300

    Added Pinia vue store

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/index.css";
 import i18n from "./i18n";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
+app.use(createPinia());
 app.use(i18n);
 app.use(router);
 app.mount("#app");
