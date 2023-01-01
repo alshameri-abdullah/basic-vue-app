b40822b14dda76410a68d20ac8f6585c57a2676e
Abdullah Al-Shamiry <alshameriaf@gmail.com>
2023-01-01 18:05:21 +0300

    Feat | Add eslint plugin for vue-i18n

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,9 +7,16 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier',
+    'plugin:@intlify/vue-i18n/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   env: { browser: true, node: true, es6: true },
+  settings: {
+    'vue-i18n': {
+      localeDir: './src/locales/*.{json,json5,yaml,yml}',
+      messageSyntaxVersion: '^9.2.2',
+    },
+  },
 }
